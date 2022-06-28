e5a77a33c58ed223fb737ef51c461a533fe308bf
jskwak <max6383@naver.com>
2022-06-28 17:14:35 +0900

    Servlet & MVC

--- src/test/java/hello/servlet/domain/member/MemberRepositoryTest.java
+++ src/test/java/hello/servlet/domain/member/MemberRepositoryTest.java
@@ -4,6 +4,10 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
 class MemberRepositoryTest {
 
     MemberRepository memberRepository = MemberRepository.getInstance();
@@ -18,15 +22,27 @@ class MemberRepositoryTest {
     void save(){
         //given
         Member member = new Member("name",30);
-
         //when
         Member savedMember = memberRepository.save(member);
-        //then
-
         Member findMember = memberRepository.findById(savedMember.getId());
+        //then
+        assertThat(savedMember).isEqualTo(findMember);
+    }
 
+    @Test
+    void findAll(){
+        //given
+        Member member1 = new Member("junseo", 30);
+        Member member2 = new Member("mikyong",30);
 
-        Assertions.assertThat(savedMember).isEqualTo(findMember);
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+        //when
+        List<Member> memberList = memberRepository.findAll();
+        //then
+        assertThat(memberList.size()).isEqualTo(2);
+        assertThat(memberList).contains(member1,member2);
+
     }
 
 
